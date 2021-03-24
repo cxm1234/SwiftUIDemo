708b1a12e1ed7d93873acc0f291fa2e48828a958
245413040 <245413040@qq.com>
2021-03-24 08:15:43 +0800

    01-Demo

---
+++ SwiftUI_Demo_01/SwiftUI_Demo_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo_01
+//
+//  Created by xiaoming on 2021/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Demo_01/SwiftUI_Demo_01/SwiftUI_Demo_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Demo_01App.swift
+//  SwiftUI_Demo_01
+//
+//  Created by xiaoming on 2021/3/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Demo_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
